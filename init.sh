40187b4576b019d6dbd0635a9821ec690dbbec3c
Ignacio Perez <ignacio.perez2123@protonmail.com>
2025-02-17 16:07:38 -0300

    Init project

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Copy .env.sample to .env"
+cp .env.sample .env
+
+echo "Generating InfluxDB environment variables"
+
+INFLUX_TOKEN=$(openssl rand -base64 32)
+sed -i "s/{influx_password}/$(openssl rand -base64 8)/g" .env
+sed -i "s/{influx_token}/${INFLUX_TOKEN}/g" .env
+sed -i "s/{influx_retention}/4w/g" .env
+sed -i "s/{influx_org}/ignis/g" .env
+sed -i "s/{influx_bucket}/monitoring/g" .env
+sed -i "s/{influx_username}/admin/g" .env
+
+echo "Create InfluxDB volumes"
+mkdir -p influxdb-data
+chmod 777 influxdb-data
+
+echo "Create Grafana volumes"
+mkdir -p grafana-data
+chmod 777 grafana-data
+
+echo "Copy Telegraf configuration"
+cp config/telegraf.conf.sample config/telegraf.conf
+sed -i "s/{influx_token}/${INFLUX_TOKEN}/g" config/telegraf.conf
+sed -i "s/{influx_org}/ignis/g" config/telegraf.conf
+sed -i "s/{influx_bucket}/monitoring/g" config/telegraf.conf
+
